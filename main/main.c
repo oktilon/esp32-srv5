278ee655f145e15ab9c2208694637b128d3a697d
Denys Stovbun <denis.stovbun@lanars.com>
2022-10-25 12:26:52 +0300

    wifi STA mode

--- main/main.c
+++ main/main.c
@@ -27,6 +27,9 @@
 #define UART_RX_PIN         GPIO_NUM_5
 #define UART_BUFFER_SIZE    128
 
+#define STA_SSID            "Lanars_Community"
+#define STA_PASS            "youshouldbehere!"
+
 static const char *TAG = "esp-server";
 
 uint8_t ledIsOn = 0;
@@ -269,6 +272,51 @@ void wifi_init_softap(void) {
              WIFI_SSID, WIFI_PASS, WIFI_CHANNEL);
 }
 
+static void wifi_sta_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    }
+}
+
+/* Initialize Wi-Fi as sta and set scan method */
+static void wifi_init_sta(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_sta_event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_sta_event_handler, NULL, NULL));
+
+    // Initialize default station as network interface instance (esp-netif)
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    // Initialize and start WiFi
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid               = STA_SSID,
+            .password           = STA_PASS,
+            .scan_method        = WIFI_FAST_SCAN, // WIFI_ALL_CHANNEL_SCAN
+            .sort_method        = WIFI_CONNECT_AP_BY_SIGNAL,
+            .threshold.rssi     = -127,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
 void uart_init() {
     const uart_config_t uart_config = {
         .baud_rate  = 115200,
@@ -287,7 +335,8 @@ void uart_init() {
 
 void app_main(void) {
     ESP_ERROR_CHECK(nvs_flash_init());
-    wifi_init_softap();
+    // wifi_init_softap();
+    wifi_init_sta();
     uart_init();
     // GPIO Init
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
